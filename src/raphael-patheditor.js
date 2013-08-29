ee685d76b9b97a2611542a11d345b49e4d0a13b5
cancerberoSgx <sebastigurin@gmail.com>
2013-08-28 22:12:11 -0300

    m2

--- src/raphael-patheditor.js
+++ src/raphael-patheditor.js
@@ -36,7 +36,7 @@ pathEd.setChangeEvent(function(ctx){
     }; 
     
 	var extendObject=function(children, parent) {
-		for(var i in parent) { //don't want to use , hasOwnProperty()
+		for(var i in parent) { //don't use hasOwnProperty()
 			children[i] = parent[i]; 
 		}
 	}
@@ -44,17 +44,28 @@ pathEd.setChangeEvent(function(ctx){
 	/**
 	 * @class EditorContext - returned to the user when an editor is installed in a shape, 
 	 * It serves as the main entry point for talking to that shape's installed editor. 
+	 * @property pathObject
+	 * @property shape 
+	 * @property commandEditors a configuration for each command type like bgCOlor, textColor, description, name. See variable commandEditors
+	 * @property editorSet a raphael set of all the path commands editor-shapes (the points)
+	 * @property legendSet the legend shapes. 
+	 * @property dragThrottle number (in ms)
 	 */
 	var EditorContext = function(context){
 		extendObject(this, context); 
 	}; 
 	EditorContext.prototype =  {
 		/**
-		 * 
 		 * the user is responsible of triggering this handler when (user defined) action is performed
+		 * @method createNewCommand
 		 * @param createCommand an object with the following information: {index: 5, cmd: ["M", 1, 2] }
 		 */
-        doCreateNewCommand: function(createCommand) {
+		createNewCommand: function(cmd) {
+			if(!cmd || !cmd.cmd || index<0 || index>this.pathObject.length)
+				return;
+			var newCmdShape = buildPathCmdEditorFor(this, cmd.cmd);
+			//now update the "model": first start with the shapeCommand, then rebuild the path shape
+			this.pathObject.splice(cmd.index, 0, cmd.cmd);
         }
 		/**
 		 * register a function that will be notified when this editor model's change
@@ -62,6 +73,16 @@ pathEd.setChangeEvent(function(ctx){
     ,	setChangeListener: function(fn) {    		
     	}
     }; 
+	
+
+	/**
+	 * removes all this set's shapes from the paper. 
+	 */
+	Raphael.st.remove = function() {
+		this.forEach(function(shape, index){
+			shape.remove();
+		}); 
+	}
     
     /**
      * installs a new path editor on this path. 
@@ -78,24 +99,25 @@ pathEd.setChangeEvent(function(ctx){
         cfg = !cfg ? {} : cfg; 
         if(!this.type || this.type!="path" || !this.attr("path")) 
             return; 
-        var pathObject = Raphael.parsePathString(this.attr("path")); 
-        
+        var pathObject = Raphael.parsePathString(this.attr("path"));         
        
         var editorSet = this.paper.set(), 
             legendSet=this.paper.set(); 
 
         var context = {
-//    		"editorSet": editorSet,
-//    		"legendSet": legendSet,
+    		"editorSet": editorSet,
+    		"legendSet": legendSet,
             "pathObject": pathObject, 
             "shape": this, 
             "commandEditors" : cfg.commandEditors ? cfg.commandEditors : commandEditors, 
-            "dragThrottle": cfg.dragThrottle ? dragThrottle : 80
+            "dragThrottle": cfg.dragThrottle ? dragThrottle : 80,
+            legendVSeparation: cfg.legendVSeparation ? legendVSeparation : 25
         }
         
         for ( var i = 0; i < pathObject.length; i++) {
             var cmd = pathObject[i]; 
             var cmdEditShape = buildPathCmdEditorFor(context, cmd); 
+            editorSet.push(cmdEditShape);
         }
         context['editorSet']=editorSet;
         
@@ -103,17 +125,37 @@ pathEd.setChangeEvent(function(ctx){
         var y = 10; 
         for(var i in context.commandEditors) {
             paper.circle(10, y, 5).attr({fill: context.commandEditors[i]["bgColor"]}); 
-            paper.text(30, y, context.commandEditors[i]["description"]).attr({"text-anchor": "start"}); 
-            y+=25; 
+            var legendShape = paper.text(30, y, context.commandEditors[i]["description"]).attr({"text-anchor": "start"});
+            legendSet.push(legendShape);
+            y+=context.legendVSeparation; 
         }
         context['legendSet']=legendSet;
         
         var ctx = new EditorContext(context); 
         this.__editorCtx=ctx;        
+//        debugger;
         return ctx; 
     }; 
-        
+    
+    /**
+     * uninstalls a path editor already installed in this shape, if any. 
+     * @method uninstallPathEditor
+     * @return the old EditorContext with shapes currently removed from paper. 
+     */
+    Raphael.el.uninstallPathEditor = function(){
+    	var ctx = this.__editorCtx; 
+    	if(ctx) {
+    		ctx.editorSet.remove();
+    		this.__editorCtx=null; 
+    	}
+    	return ctx;
+    }
+    Raphael.el.hidePathEditor = function(){
+    	
+    }
+    
     var buildPathCmdEditorFor = function(context, cmd) {
+    	
         if(!cmd ||cmd.length<1)
             return; 
         
@@ -153,8 +195,12 @@ pathEd.setChangeEvent(function(ctx){
     var buildLineToCmdEditor = function(context, cmd) {
         var paper = context.shape.paper; 
         var c = createCmdShape([cmd[1], cmd[2]], context, cmd, "L");//c = paper.circle(cmd[1], cmd[2], 5).attr({fill: context.commandEditors["L"]["bgColor"]}); 
-        var ctx = {"pathObject": context.pathObject, "shape": context.shape, 
-            "feedback": c, "cmd": cmd}; 
+        var ctx = {
+        	"pathObject": context.pathObject, 
+        	"shape": context.shape, 
+            "feedback": c, 
+            "cmd": cmd
+        }; 
         c.drag(function_throttle(context.dragThrottle, 
             function(dx, dy){ /*move*/
                 var x = this.ox+dx, y = this.oy+dy; 
@@ -202,19 +248,23 @@ pathEd.setChangeEvent(function(ctx){
     var buildHLineToCmdEditor = function(context, cmd) {
         var paper = context.shape.paper; 
         var c = createCmdShape([cmd[1], 50], context, cmd, "H");//paper.circle(cmd[1], 50, 5).attr({fill: context.commandEditors["H"]["bgColor"]});            
-        var ctx = {"pathObject": context.pathObject, "shape": context.shape, 
-            "feedback": c, "cmd": cmd}
+        var ctx = {
+        	"pathObject": context.pathObject, 
+        	"shape": context.shape, 
+//            "feedback": c, 
+            "cmd": cmd
+        }
         c.drag(function_throttle(context.dragThrottle, 
             function(dx, dy){ /*move*/
                 var x = this.ox+dx, y = this.oy+dy; 
                 this.cmd[1]=x; 
 //                this.cmd[2]=y;
-                this.feedback.attr({"cx": x, "cy": y});
+                this.shape.attr({"cx": x, "cy": y});
                 this.shape.attr({"path": this.pathObject}); 
             }),
             function(x, y){ /*drag start*/
-                this.ox=this.feedback.attr("cx"); 
-                this.oy=this.feedback.attr("cy");
+                this.ox=this.shape.attr("cx"); 
+                this.oy=this.shape.attr("cy");
             },  function(){ /*ends*/
                 
             }, ctx, ctx, ctx
@@ -402,3 +452,56 @@ pathEd.setChangeEvent(function(ctx){
     
 })(); 
 
+
+
+
+
+//rapahel extension idea sidenote
+
+///**
+// * raphael shapes can be reused in multiple papers or remove()d and added again easily with this method
+// * @returns a new shape, based on self, rendered on te given papaer. Notice that this shape belongs to another paper's than self's.
+// */
+//Raphael.el.render = function(paper) {
+//	if(this.type=="rect") {
+//		
+//	}
+//	if(this.type=="ellipse") {
+//				
+//	}
+//	if(this.type=="rect") {
+//		
+//	}
+//	if(this.type=="rect") {
+//		
+//	}
+//	if(this.type=="rect") {
+//		
+//	}
+//} 
+//
+
+/*
+Checkout Error Management. 
+
+The logic for designing a checkout with mutltiple Steps is that, in each step, when the user presses continue, some important part of the customer's 
+order is updated in the backend. When the user presses continue, he or she expects thier data to be sended and validated by the server and somehow 
+complete that part of the checkout proccess. 
+
+In the context of a Step then, we can differentiate two classes of errors: 
+
+1) STEP LEVEL ERRORS: these are errors that mainly occurred at the backend when
+2) MODULE LEVEL ERRORS  
+a relative big update is done 
+in the customer's order. This means some portion of the or
+
+
+preguntas / conclusiones de checkout
+ 
+1) generica - errores: 
+
+
+para kf: cada módulo, por lo gral solo actualiza el modelo y delega en super.submit() para que sea el Step el encargado de hacer el commit gral y mostrar errores. ejemplo invoice
+por otro lado hay modulos que si necesitarán hacer ajax....
+
+*/
